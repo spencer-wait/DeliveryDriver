83dba34aa0555198d6cedaaa42d1ce21b38e7073
Spencer Wait <spencer.wait@gmail.com>
2022-12-12 16:33:54 -0500

    Removed boost and slow down system

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,23 +5,15 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float initMoveSpeed = 10f;
-    [SerializeField] float boostSpeed = 20f;
-    [SerializeField] float slowSpeed = 5f;
-    float currentSpeed;
+    [SerializeField] float moveSpeed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentSpeed = initMoveSpeed;
-    }
 
     // Update is called once per frame
     void Update()
     {
         // assign input to steering and driving
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float drivingSpeed = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
+        float drivingSpeed = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         // perform more realistic car movements
         if (drivingSpeed == 0) { transform.Rotate(0, 0, 0); } // Don't allow turning when car is stopped
@@ -29,20 +21,6 @@ public class Driver : MonoBehaviour
         else { transform.Rotate(0, 0, -steerAmount); }  // rotate car based on negative horizontal axis when moving forward
 
         transform.Translate(0, drivingSpeed, 0);    // move car forward/back based on vertical axis
-
         
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Boost")
-        {
-            currentSpeed = boostSpeed;
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        currentSpeed = slowSpeed;
     }
 }
